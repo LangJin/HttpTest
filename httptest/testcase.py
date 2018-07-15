0df121c7eddf9281a1da81d0249c74d99bc0b879
testjie <snake_goodluck@hotmail.com>
2018-07-15 21:07:53 +0800

    add testcase validate

--- httptest/testcase.py
+++ httptest/testcase.py
@@ -19,7 +19,6 @@ def _validate_some_items(items):
             (someone, dict),
             (sometwo, str)
         ]
-        allow_none:是否允许值为空
     :return: False:校验失败；True:校验成功
     """
     for item in items:
@@ -54,125 +53,10 @@ def import_json_file(filename):
         exit()
 
 
-# todo 等最终的格确定了做这里
-def _basic_format_validate(case):
-    """
-    :校验标准测试用例格式
-        标准格式如下:
-        {
-                "request": "{"url": "http://127.0.0.1:2333/test", "json": {"aaa": "bbb"}, "method": "POST", "headers": {"Content-Type": "application/json"}, "timeout": 10}",
-                "testname": "用例99",
-                "testtype": "testcass",
-                "validate": "[{"Equal": ["r.json()","request["json"]"]},{"Equal": ["r.status_code","200"]}]"
-        }
-    :param case:
-            校验格式
-             request:
-             1.request不能为空;url不能为空
-             2.method不能为空:get/post；
-             3.header不能为空，只能为json；
-             4.json不能为空；
-             5.timeout不能只能为数字，大于0
-
-             testname:
-             1.不能为空
-
-             testtype:
-             1.不能为空
-             2.testcass和testsuite
-
-             validate：
-             1.不能为空
-    :return: False失败,True成功
-
-     """
-    if not isinstance(case, dict):
-        try:
-            case = json.loads(case, encoding="utf-8")
-        except Exception as e:
-            return False
-
-    # request
-    try:
-        request = json.loads(case["request"], encoding="utf-8")
-        # request
-        if request is None or \
-                not isinstance(request, dict) or request == {}:
-            return False
-
-        # url
-        if request["url"] is None or \
-                not isinstance(request["url"], str) or request["url"] == "":
-            return False
-
-        # method
-        if request["method"] is None or \
-                not isinstance(request["method"], str):
-            return False
-        if request["method"] not in ("get", "post", "GET", "POST"):
-            return False
-
-        # timeout
-        if request["timeout"] is None or \
-                not isinstance(request["timeout"], int) or request["timeout"] == "":
-            return False
-
-        # json
-        if request["json"] is None or \
-                not isinstance(request["json"], dict) or request["json"] == {}:
-            return False
-
-        # headers
-        if request["headers"] is None or \
-                not isinstance(request["headers"], dict) or request["headers"] == {}:
-            return False
-        if "Content-Type" not in request["headers"].keys() \
-                and "application/json" not in request["headers"].values():
-            return False
-    except (Exception, KeyError, SyntaxError) as e:
-        return False
-
-    # testname
-    try:
-        testname = case["testname"]
-        if testname is None or \
-                not isinstance(testname, str) or testname == "":
-            return False
-    except (Exception, KeyError, SyntaxError) as e:
-        return False
-
-    # testtype
-    try:
-        testtype = case["testtype"]
-        if testtype is None or \
-                not isinstance(testtype, str) or testtype == "":
-            return False
-        if testtype not in ("testcass", "testsuite"):
-            return False
-    except (Exception, KeyError, SyntaxError) as e:
-        return False
-
-    # validate
-    # todo validate str to dict异常，需要解决这个问题！！
-    try:
-        validate = case["validate"]
-        if validate is None or \
-                not isinstance(validate, str) or validate == "":
-            return False
-
-    except (Exception, KeyError, SyntaxError) as e:
-        return False
-
-    return True
-
-
-
-
 def validate_json_case(filename):
     """
     检查json文件的内容
     {
-        "testcase": {
             "name": "teatname",
             "request": {
                 "url": "http://127.0.0.1/test",
@@ -191,48 +75,56 @@ def validate_json_case(filename):
             "validate": [
                 {"eq": ["status_code", 200]}
                 ]
-            }
     }
     """
+    valid_items = []
     try:
-        # 读取json文件
-        with open(filename, 'r') as f:
+        # 读取json文件并生成json对象
+        with open(filename, 'r', encoding="utf-8") as f:
             json_obj = json.load(f)
 
-        # 校验testcase/extract/validate是否存在
-        # extract = json_obj["extract"]
-        testcase = json_obj["testcase"]
-        # validate = json_obj["validate"]
-
-        # 校验testcase/name;testcase/name;
-        testcase_name = testcase["name"]
-        testcase_request = testcase["request"]
-        testcase_request_url = testcase_request["url"]
-        testcase_request_json = testcase_request["json"]
-        testcase_request_method = testcase_request["method"]
-
-        # 校验testcase/request/headers/content-type的各级内容
-        testcase_request_headers = testcase_request["headers"]
-        testcase_request_headers_content = testcase_request_headers["Content-Type"]
+        # 检查testcase项, 默认每个key是必填，如果不存在某个key时直接获取则会抛出异常
+        t_request = eval(json_obj["request"])                           # testcase/request
+        t_request_url = t_request["url"]                                # testcase/request/url
+        t_request_json = t_request["json"]                              # testcase/request/json
+        t_request_method = t_request["method"]                          # testcase/request/method
+        t_request_headers = t_request["headers"]                        # testcase/request/headers
+        t_request_headers_content = t_request_headers["Content-Type"]   # testcase/request/headers/content-type
 
         # 添加所有需要校验的items
-        valids = []
-        valids.append((testcase_name, str))
-        valids.append((testcase_request, dict))
-        valids.append((testcase_request_url, str))
-        valids.append((testcase_request_method, str))
-        valids.append((testcase_request_headers, dict))
-        valids.append((testcase_request_headers_content, str))
+        valid_items.append((t_request, dict))
+        valid_items.append((t_request_url, str))
+        valid_items.append((t_request_method, str))
+        valid_items.append((t_request_headers, dict))
+        valid_items.append((t_request_headers_content, str))
+        valid_items.append((t_request_json, dict, True))     # 允许json内容为空
 
-        # json允许为空
-        valids.append((testcase_request_json, dict, True))
-
-        # 校验失败，跑出异常由try捕获统一处理
-        if not _validate_some_items(valids):
+        # request各级
+        if not _validate_some_items(valid_items):
+            logger.error("【错误】: 测试用例的request模块校验异常，请检查该模块")
             raise Exception
 
-    except (Exception, exception.NotFoundCaseError):
-        raise exception.NotFoundCaseError("用例格式错误,请检查测试用例格式")
+        # name
+        name = json_obj["name"]
+        if not _validate_some_items([(name, str)]):
+            logger.error("【错误】: 测试用例的name模块校验异常，请检查该模块")
+            raise Exception
+
+        # extract
+        extract = eval(json_obj["extract"])
+        if not _validate_some_items([(extract, list, True)]):
+            logger.error("【错误】: 测试用例的extract模块校验异常，请检查该模块")
+            raise Exception
+
+        # validate
+        validate = json_obj["validate"]
+        if not _validate_some_items([(validate, list, True)]):
+            logger.error("【错误】: 测试用例的validate模块校验异常，请检查该模块")
+            raise Exception
+
+    except (Exception, exception.NotFoundCaseError, exception.ValidateError):
+        logger.error("【错误】：校验测试用例时发生异常，测试用例校验不通过")
+        raise exception.ValidateError("【错误】：校验测试用例时发生异常，测试用例校验不通过")
 
     return json_obj
 
